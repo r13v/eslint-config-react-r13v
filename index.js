5e2ed167356a234eaa98b629c90da6a605647ee1
Rustam Gilyaziev <gilaziev@gmail.com>
2020-05-25 15:36:57 +0400

    feat: do not require return type

--- index.js
+++ index.js
@@ -107,6 +107,7 @@ module.exports = {
     "@typescript-eslint/no-empty-interface": "warn",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
 
     // prettier
     "prettier/prettier": [
