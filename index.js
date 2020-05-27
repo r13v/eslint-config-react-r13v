e6630cd8ff464d5ab5528812b59c4737a8f07a93
Rustam Gilyaziev <gilaziev@gmail.com>
2020-05-27 17:43:25 +0400

    feat: ignore variables with _ (underscore) prefix

--- index.js
+++ index.js
@@ -133,8 +133,15 @@ module.exports = {
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-var-requires": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-unused-vars": ["warn", { varsIgnorePattern: "^_+" }],
-  
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        varsIgnorePattern: "^_+",
+        argsIgnorePattern: "^_+",
+        ignoreRestSiblings: true,
+      },
+    ],
+
     // prettier
     "prettier/prettier": [
       "warn",
